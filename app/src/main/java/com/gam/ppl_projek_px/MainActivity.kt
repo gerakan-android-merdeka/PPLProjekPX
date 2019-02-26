708778fb4d76d5ce9e9e4f05edc037b13d99b1aa
bintangeditya <bintangfarhandika@gmail.com>
2019-02-26 16:21:43 +0700

    menambahkan intent ke BActivity

--- app/src/main/java/com/gam/ppl_projek_px/MainActivity.kt
+++ app/src/main/java/com/gam/ppl_projek_px/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.gam.ppl_projek_px
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when (v){
+            button->{
+                val intent = Intent(this, BActivity::class.java)
+                startActivity(intent)
+            }
+            button2->{}
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button.setOnClickListener(this)
+        button2.setOnClickListener(this)
     }
 }
