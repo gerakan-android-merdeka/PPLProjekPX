0d3ba39b93cdbcf70ddfe13fd585256bae72a7c0
Teguh Siswanto <tsiswanto73@gmail.com>
2019-02-27 18:01:58 +0700

    menambahkan bottom navbar

--- app/src/main/java/com/gam/ppl_projek_px/MainActivity.kt
+++ app/src/main/java/com/gam/ppl_projek_px/MainActivity.kt
@@ -3,37 +3,60 @@ package com.gam.ppl_projek_px
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.Menu
 import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.sql.SQLOutput
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        when (v){
-            button->{
-                val intent = Intent(this, BActivity::class.java)
-                startActivity(intent)
+
+    private var content: FrameLayout? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.home_menu -> {
+                val fragment = HomeFragment.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
             }
-            button2->{}
+            R.id.listen_menu-> {
+                val fragment = ListenFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.news_menu-> {
+                val fragment = NewsFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
         }
+        false
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+            .commit()
+    }
+
+    override fun onClick(v: View?) {
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        button.setOnClickListener(this)
-        button2.setOnClickListener(this)
-        println("bintang")
-        println("Hai")
-        println("bintang")
-        println("Hai")
-        println("bintang")
-        println("Hai")
-        println("bintang")
-        println("Hai")
-
-        println("waw")
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val fragment = HomeFragment.newInstance()
+        addFragment(fragment)
     }
+
+
 }
